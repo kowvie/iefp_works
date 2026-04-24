4717465603ccdedaef276b50c528b53151f09e56
kowvie <etoilebrause@gmail.com>
2026-04-24 10:34:08 +0100

    barbies_closet
    fixed html, views and models

--- myproject/barbies_closet/views.py
+++ myproject/barbies_closet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Peca
 from .models import Outfit
 from .models import Categoria, Estacao
@@ -14,8 +14,9 @@ def criar_peca(request):
         marca = request.POST['marca']
         tamanho = request.POST['tamanho']
         data = request.POST['data']
-        categoria = Categoria.objects.get(id=request.POST['categoria'])
-        estacao = Estacao.objects.get(id=request.POST['estacao'])
+
+        categoria = get_object_or_404(Categoria, id=request.POST['categoria'])
+        estacao = get_object_or_404(Estacao, id=request.POST['estacao'])
 
         Peca.objects.create(
             utilizador=request.user,
@@ -32,28 +33,30 @@ def criar_peca(request):
     categorias = Categoria.objects.all()
     estacoes = Estacao.objects.all()
 
-    return render(request, 'pecas/criar.html', {
+    return render(request, 'pecas/pecas_criar.html', {
         'categorias': categorias,
         'estacoes': estacoes
     })
 
 def lista_outfits(request):
     outfits = Outfit.objects.filter(utilizador=request.user)
-    return render(request, 'outfits/lista.html', {'outfits': outfits})
+    return render(request, 'outfit/outfit_lista.html', {'outfits': outfits})
 
 def criar_outfit(request):
     if request.method == 'POST':
         nome = request.POST['nome']
         pecas_ids = request.POST.getlist('pecas')
 
+        pecas = Peca.objects.filter(utilizador=request.user, id__in=pecas_ids)
+        if pecas.count() != len(pecas_ids):
+            return redirect('criar_outfit')
+
         outfit = Outfit.objects.create(
             nome=nome,
             utilizador=request.user
         )
-
-        outfit.pecas.set(pecas_ids)
-
+        outfit.pecas.set(pecas)
         return redirect('lista_outfits')
 
     pecas = Peca.objects.filter(utilizador=request.user)
-    return render(request, 'outfits/criar.html', {'pecas': pecas})
+    return render(request, 'outfit/outfit_criar.html', {'pecas': pecas})
