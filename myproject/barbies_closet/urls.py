f2d60c0bb1f046ddf2f936f8316af5f7ead92998
Jessica Sofia Silva Ribeiro <jsilva-r@c1r16s2.42porto.com>
2026-04-26 01:29:19 +0100

    ola

--- myproject/barbies_closet/urls.py
+++ myproject/barbies_closet/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.lista_pecas),
     path('pecas/', views.lista_pecas, name='lista_pecas'),
     path('pecas/criar/', views.criar_peca, name='criar_peca'),
-    path('outfits/', views.lista_outfits, name='lista_outfits'),
-    path('outfits/criar/', views.criar_outfit, name='criar_outfit'),
-    path('outfits/<int:id>/', views.detalhe_outfit, name='detalhe_outfit'),
+    path('outfit/', views.lista_outfits, name='lista_outfits'),
+    path('outfit/criar/', views.criar_outfit, name='criar_outfit'),
+    path('outfit/<int:id>/', views.detalhe_outfit, name='detalhe_outfit'),
 ]
